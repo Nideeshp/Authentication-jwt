0c6a06fef80103d295ed7ec347299b7fec9dd9e2
nideesh <nideeshnd313@gmail.com>
2023-11-02 08:30:40 +0530

    internal server error added

--- constants.js
+++ constants.js
@@ -3,10 +3,12 @@ exports.constants={
     UNAUTHORIZED:401,
     FORBIDDEN:403,
     NOT_FOUND:404,
+    INTERNAL_SERVER_ERROR:500,
     REQUEST_TIMEOUT:408,
     SERVER_ERROR:500,
     NO_CONTENT:204,
     OK:200,
+
 
     
 
