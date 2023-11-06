4d847ace2f8339d6f9756f5f46b6359997d6fb99
nideesh <nideeshnd313@gmail.com>
2023-11-06 15:27:29 +0530

    request timeout removed

--- constants.js
+++ constants.js
@@ -4,10 +4,10 @@ exports.constants={
     FORBIDDEN:403,
     NOT_FOUND:404,
     INTERNAL_SERVER_ERROR:500,
-    REQUEST_TIMEOUT:408,
     SERVER_ERROR:500,
     NO_CONTENT:204,
     OK:200,
+
 
 
     
