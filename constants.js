30e5dab58a87a00c158ef891d384019f934e0788
nideesh <nideeshnd313@gmail.com>
2023-11-30 11:42:56 +0530

    LOCKED REMOVED

--- constants.js
+++ constants.js
@@ -8,10 +8,9 @@ exports.constants={
     BAD_GATEWAY:502,
     SERVICE_UNAVAILABLE:503,
     GATEWAY_TIMEOUT:504,
-    LOCKED:423,
     PRECONDITION_FAILED:412,
     UNPROCESSABLE_ENTITY:422,
-    
+
     
     
     
