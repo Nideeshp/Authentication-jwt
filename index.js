03cb5a69b251c467ec522e0fd1f43276685d27e2
nideesh <nideeshnd313@gmail.com>
2023-12-20 18:07:45 +0530

    another commit

--- index.js
+++ index.js
@@ -13,11 +13,5 @@ app.use('/api/contacts',require('./routes/contactsroutes'))
 app.use('/api/users',require('./routes/usersroutes'))
 app.use(errorhandler)
 
-
-
-
-
-
-    
 app.listen(port,()=>console.log(`Server is running port ${port}`))
 
